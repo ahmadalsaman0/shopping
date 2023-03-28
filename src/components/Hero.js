bd49476d937ad2deed9b23577c69a8df4ddc7f5d
ahmadalsaman0 <ahmad.alsamman@std.yildiz.edu.tr>
2023-03-28 16:45:40 +0300

    v1

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,26 @@
-import React from 'react';
-
+import React from "react";
+import {Link }from 'react-router-dom'
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="h-[400px] bg-no-repeat bg-cover bg-center py-24">
+      <div className="container mx-auto flex justifya-around h-full">
+      <div className="flex flex-col justify-center">
+        <div class='font-semibold flex items-center uppercase' >
+          <div className="w-10 h-[2px] bg-red-500 mr-3"></div>New trend
+        </div>
+        <h1 className="text-[70px] leading-[1.1] font-light mb-4">AUTUMN SALE STYLISH <br />
+        <span className="font-semibold flex items-center uppercase">  WOMENS </span>
+         </h1>
+         <Link to={'/'} className='self-start uppercase font-semibold border-b-2 border-primary'>
+         Discover More</Link>
+      </div>
+<div className="hidden lg:block ">
+
+    <img src="https://images.unsplash.com/photo-1607083206968-13611e3d76db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2FsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" />
+    </div>
+    </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
-import React from 'react';
-
+import React, { useContext } from "react";
+import Product from "../components/Product";
+import { ProductContext } from "../contexts/ProductContext";
+import Hero from "../components/Hero";
 const Home = () => {
-  return <div>Homepage</div>;
+  const { products } = useContext(ProductContext);
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category === "men's clothing" || item.category === "women's clothing"
+    );
+  });
+  return (
+    <div>
+      <Hero />
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div
+            className="grid grid-clos-1 md:grid-cols-2
+          lg:grid-cols-4 xl:grid-cols-5 gap-[30px] md:max=w=none md:mx-0"
+          >
+            {filteredProducts.map((product) => {
+              return <Product product={product} key={product.id} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
